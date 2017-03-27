d04e3e2ac0266bcd41e8df4316037ff07d27df71
smikulcik <smikulcik@gmail.com>
2017-03-27 17:10:50 -0400

    Add basic planimeter from 2012

---
+++ src/planimeter/PointD.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package planimeter;
+
+import java.awt.*;
+import java.awt.geom.*;
+/**
+ *
+ * @author Mikulcik
+ */
+public class PointD implements Comparable<PointD>
+{
+        private double x;
+	private double y;
+	
+	public PointD(){
+		x = 0;
+		y = 0;
+	}
+	public PointD(double xPos, double yPos){
+		x = xPos;
+		y = yPos;
+	}
+        
+        public void draw(Graphics2D g){
+            g.fill(new Ellipse2D.Double(x-2,y-2,4,4));
+        }
+        public double getX(){return x;}
+        public double getY(){return y;}
+        public void set(PointD point){x=point.getX(); y=point.getY();}
+        public void setX(double newX){x = newX;}
+        public void setY(double newY){y = newY;}
+        
+        public PointD rotatedAround(PointD center, double degrees){
+            double newX = center.getX() + ((x-center.getX())*Math.cos(degrees*Math.PI/180) - (y-center.getY())*Math.sin(degrees*Math.PI/180));
+            double newY = center.getY() + ((x-center.getX())*Math.sin(degrees*Math.PI/180) + (y-center.getY())*Math.cos(degrees*Math.PI/180));
+            
+            return new PointD(newX,newY);
+        }
+        
+        public double angleTo(PointD pt){
+            double angle = Math.atan((pt.getY()-y)/(pt.getX()-x))*180/Math.PI;
+            if(angle<0)angle+=360;
+            if(pt.getX()-x<0){angle+=180;}
+            while(angle>360)angle-=360;
+            return angle;
+       
+        }
+       
+        @Override
+        public int compareTo(PointD other){
+            if(other.getX()==x &&other.getY() == y){
+                return 0;
+            }else{
+                return -1;
+            }
+        }
+        public boolean equals(PointD other){
+            if(other.getX()==x &&other.getY() == y){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        @Override
+        public String toString(){return "(" + (int)x + "," + (int)y + ")";}
+        
+}//end PointD
